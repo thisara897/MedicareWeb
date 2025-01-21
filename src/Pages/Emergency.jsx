c8f2a0c5ac1c15e176d6b2d9ff16404670713ca1
thisara <warshangunasinghe345@gmail.com>
2025-01-21 22:02:43 +0530

    update emergency page

--- src/Pages/Emergency.jsx
+++ src/Pages/Emergency.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
-function Emergency() {
+import HospitalBed from '../assets/HospitalBed.jpg'
+import './Emergency.css'
+const Emergency = () => {
   return (
-    <div>
-      <p>This is Emergency</p>
+    <div className='EMG_page'>
+      <div className="TextSection">
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus pariatur esse amet dolorum ipsam 
+          autem labore corporis,
+           provident quis beatae, delectus ratione, recusandae qui tenetur a laborum perspiciatis iure voluptas.
+        </p>
+        <button>Call 911</button>
+
+      </div>
+      <div className="imageSection">
+        <img src={HospitalBed} alt = "Hospital Bed"/>
+      </div>
     </div>
   )
 }
 
 export default Emergency
+
