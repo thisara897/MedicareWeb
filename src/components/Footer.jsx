96f3f59c74f6c19a138ecf779a007e49fb78956b
thisara <warshangunasinghe345@gmail.com>
2025-01-26 12:54:50 +0530

    add styles to the icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,22 @@ import { faFacebook, faWhatsapp,faInstagram, faGithub } from '@fortawesome/free-
 function Footer() {
   return (
     <div className='footer-container'>
-      <div className="first-sectionn">
-      <FontAwesomeIcon icon={faFacebook} size="2x" style={{ color: '#FFFFFF' }} className='icons'/>
-      <FontAwesomeIcon icon={faWhatsapp } size="2x" style={{ color: '#FFFFFF' }} className='icons'/>
-      <FontAwesomeIcon icon={faInstagram } size="2x" style={{ color: '#FFFFFF' }} className='icons'/>
-      <FontAwesomeIcon icon={faGithub} size="2x" style={{ color: '#FFFFFF' }} className='icons'/>
+      <div className="Left-section">
+        <p>Medicare Hospital colombo Road,<br/> Negombo </p>
+      </div>
+      <div className="Right-section">
+        <div className="icon-container">
+          <FontAwesomeIcon icon={faFacebook} size="2x"  className='icons facebook'/><span className='icon-name facebook'>Facebook</span>
+        </div>
+        <div className="icon-container">
+          <FontAwesomeIcon icon={faWhatsapp } size="2x"  className='icons  whatsapp'/><span className='icon-name whatsapp'>Whats app</span>
+        </div>
+        <div className="icon-container">
+          <FontAwesomeIcon icon={faInstagram } size="2x"  className='icons instagram'/><span className='icon-name instagram'>Instergram</span>
+        </div>
+        <div className="icon-container">
+          <FontAwesomeIcon icon={faGithub} size="2x" style={{ color: '#FFFFFF' }} className='icons'/><span className='icon-name github'>Github</span>
+        </div>        
       </div>
     </div>
   )
